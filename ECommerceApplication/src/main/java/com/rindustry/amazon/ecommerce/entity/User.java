9336d53e72588b52d855468b544d4cc4f957fb77
vipulrane <vipulrane1@gmal.com>
2025-05-03 12:49:18 +0530

    User class field added

--- ECommerceApplication/src/main/java/com/rindustry/amazon/ecommerce/entity/User.java
+++ ECommerceApplication/src/main/java/com/rindustry/amazon/ecommerce/entity/User.java
@@ -1,8 +1,71 @@
 package com.rindustry.amazon.ecommerce.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "user")
 public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id")
+	Integer user_id;
+	@Column(name = "user_name")
+	String user_name;
+	@Column(name = "user_email")
+	String user_email;
+	@Column(name = "user_password")
+	String user_password;
+
+	public User(Integer user_id, String user_name, String user_email, String user_password) {
+		super();
+		this.user_id = user_id;
+		this.user_name = user_name;
+		this.user_email = user_email;
+		this.user_password = user_password;
+	}
+
+	public Integer getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(Integer user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getUser_name() {
+		return user_name;
+	}
+
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+
+	public String getUser_email() {
+		return user_email;
+	}
+
+	public void setUser_email(String user_email) {
+		this.user_email = user_email;
+	}
+
+	public String getUser_password() {
+		return user_password;
+	}
+
+	public void setUser_password(String user_password) {
+		this.user_password = user_password;
+	}
+
+	@Override
+	public String toString() {
+		return "User [user_id=" + user_id + ", user_name=" + user_name + ", user_email=" + user_email
+				+ ", user_password=" + user_password + "]";
+	}
 
 }
